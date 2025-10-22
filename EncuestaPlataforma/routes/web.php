315b02ebbd07070920aab2db151d84c2ed90bef9
DannyUR <111530669+DannyUR@users.noreply.github.com>
2025-10-22 13:17:44 -0600

    3.1

--- EncuestaPlataforma/routes/web.php
+++ EncuestaPlataforma/routes/web.php
@@ -8,6 +8,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
